6394975456933936f7d2c2908435c57715da8c36
Haoyan Li <angelgamer@gmail.com>
2017-04-26 20:21:25 +0900

    Modified codes for audio and video version.

--- webrtc_video_chat.js
+++ webrtc_video_chat.js
@@ -5,7 +5,7 @@
 
 var constraints = {
   video: true,
-  audio: false,
+  audio: true,
 };
 
 
@@ -13,9 +13,9 @@ var $localVideo = $('video.video#video_local')[0],
     $remoteVideo = $('video.video#video_remote')[0];
 
 var $localCtrlBtn = $('button.btn#local_video_ctrl_btn'),
-    $remoteBtn = $('button.btn#remote_video_ctrl_btn'),
-    $localInviteBtn = $('button.btn#local_video_invite_btn'),
-    $remoteInviteBtn = $('button.btn#remote_video_invite_btn');
+    // $remoteBtn = $('button.btn#remote_video_ctrl_btn'),
+    $localInviteBtn = $('button.btn#local_video_invite_btn');
+    // $remoteInviteBtn = $('button.btn#remote_video_invite_btn');
 
 var localVideo = document.getElementById('video_local');
 
@@ -51,16 +51,53 @@ $localCtrlBtn.click(function () {
 });
 
 var iceServers = {
-  iceServers: [{
-    'urls': [
-      'stun:stun.l.google.com:19302'
-    ]
-  }]
+  iceServers: [
+      {url:'stun:stun01.sipphone.com'},
+      {url:'stun:stun.ekiga.net'},
+      {url:'stun:stun.fwdnet.net'},
+      {url:'stun:stun.ideasip.com'},
+      {url:'stun:stun.iptel.org'},
+      {url:'stun:stun.rixtelecom.se'},
+      {url:'stun:stun.schlund.de'},
+      {url:'stun:stun.l.google.com:19302'},
+      {url:'stun:stun1.l.google.com:19302'},
+      {url:'stun:stun2.l.google.com:19302'},
+      {url:'stun:stun3.l.google.com:19302'},
+      {url:'stun:stun4.l.google.com:19302'},
+      {url:'stun:stunserver.org'},
+      {url:'stun:stun.softjoys.com'},
+      {url:'stun:stun.voiparound.com'},
+      {url:'stun:stun.voipbuster.com'},
+      {url:'stun:stun.voipstunt.com'},
+      {url:'stun:stun.voxgratia.org'},
+      {url:'stun:stun.xten.com'},
+      {
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com'
+      },
+      {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      },
+      {
+        url: 'turn:192.158.29.39:3478?transport=tcp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      }
+  ]
 };
 
+var options = {
+  offerToReceiveAudio: true,
+  offerToReceiveVideo: true,
+  iceRestart: true,
+  voiceActivityDetection: true
+};
 
 $localInviteBtn.click(function () {
-  peerConnection1 =  new RTCPeerConnection(iceServers);
+  peerConnection1 = new RTCPeerConnection(iceServers);
 
   peerConnection1.onicecandidate = HandleICECandidateEvent;
 
@@ -71,40 +108,44 @@ $localInviteBtn.click(function () {
   peerConnection1.addStream(stream);
 
   peerConnection1.onnegotiationneeded = function () {
-    peerConnection1.createOffer({
-      iceRestart: true,
-      voiceActivityDetection: true
-    }).then(function (offer) {
-      return peerConnection1.setLocalDescription(offer)
-    }).then(function () {
-      SendOfferAndAnswer(peerConnection1.localDescription);
-    }).catch(function (error) {
-      console.error(error);
-    })
+    peerConnection1.createOffer(options)
+      .then(function (offer) {
+        return peerConnection1.setLocalDescription(offer)
+      }).then(function () {
+        SendOfferAndAnswer(peerConnection1.localDescription);
+      }).catch(function (error) {
+        console.error(error);
+      })
   }
 });
+
 
 function HandleVideoOffer(offerPack) {
   peerConnection1 = new RTCPeerConnection(iceServers);
 
   peerConnection1.onaddstream = HandleAddStreamEvent;
+  peerConnection1.onicecandidate = HandleICECandidateEvent;
+
   // var sdp = offerPack.sdp;
   var sdp = new RTCSessionDescription(offerPack.sdp);
   var prevCaller = offerPack.caller;
 
+  peerConnection1.addStream(stream);
+
+  // peerConnection1.setRemoteDescription(sdp).then(function () {
+  //   return navigator.mediaDevices.getUserMedia(constraints);
+  // }).then(function (stream) {
+  //   videoTracks = stream.getVideoTracks();
+  //
+  //   window.stream = stream;
+  //   $localVideo.srcObject = stream;
+  //
+  //   // videoTracks.forEach(function (p1, p2, p3) {
+  //   //   this.addTrack(p1, $localVideo);
+  //   // });
+  //   peerConnection1.addStream(stream);
+  // })
   peerConnection1.setRemoteDescription(sdp).then(function () {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  }).then(function (stream) {
-    videoTracks = stream.getVideoTracks();
-
-    window.stream = stream;
-    $localVideo.srcObject = stream;
-
-    // videoTracks.forEach(function (p1, p2, p3) {
-    //   this.addTrack(p1, $localVideo);
-    // });
-    peerConnection1.addStream(stream);
-  }).then(function () {
     return peerConnection1.createAnswer();
   }).then(function (answer) {
     return peerConnection1.setLocalDescription(answer);
@@ -142,10 +183,23 @@ function HandleNewICECandidateMsg(candidatePack) {
     .catch(function (error) {
       console.error(error);
     });
-  console.log('A new ICE candidate has been added.')
-  console.log('Candidate information: ' + JSON.stringify(candidatePack.candidate));
+  console.log('A new ICE candidate has been added.');
+  console.log('Candidate information: ' +
+    JSON.stringify(candidatePack.candidate));
 }
 
 function HandleAddStreamEvent(event) {
   $remoteVideo.srcObject = event.stream;
+}
+
+function HandleICEConnectionStateEvent(event) {
+  console.info('ICE Connection State Change to: ' +
+    peerConnection1.iceConnectionState);
+
+  switch (peerConnection1.iceConnectionState) {
+    case "closed":
+    case "failed":
+    case "disconnected":
+
+  }
 }
